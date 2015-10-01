62f4de347832235677ac193b607b822b40224833
jillmsmith <jillma.smith@gmail.com>
2015-10-01 01:13:15 +0000

    Recommit Assignment 22

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,34 +1,31 @@
 require_relative "entry.rb"
  
  class AddressBook
-   attr_accessor :entries
+  attr_accessor :entries
 
-   def initialize
-      @entries = []
-   end
+  def initialize
+    @entries = []
+  end
    
-    def remove_entry(name, phone, email)
-      delete_entry = nil
-        
-      @entries.each do |entry|
-        if name == entry.name && phone == entry.phone_number &&  email == entry.email
-           delete_entry = entry
-        end
+  def remove_entry(name, phone, email)
+    @entries.each do |entry|
+      if name == entry.name && phone == entry.phone_number &&  email == entry.email
+        @entries.delete(delete_entry)
       end
-        
-      @entries.delete(delete_entry)
-        
     end
+  end
    
-   def add_entry(name, phone, email)
+  def add_entry(name, phone, email)
 
-     index = 0
-     @entries.each do |entry|
-       if name < entry.name
-         break
-       end
-       index += 1
-     end
-     @entries.insert(index, Entry.new(name, phone, email))
-   end
+    index = 0
+    @entries.each do |entry|
+      if name < entry.name
+        break
+      end
+        index += 1
+    end
+    @entries.insert(index, Entry.new(name, phone, email))
+  end
  end
+ 
+ 
